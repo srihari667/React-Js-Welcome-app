c4e612a0eeb2260496611d40d0ce2e4113756965
srihari667 <srihari667@gmail.com>
2023-03-13 04:53:03 +0000

    Welcome app solution

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,37 @@
+// Write your code here
+/* eslint-disable react/button-has-type */
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class Welcome extends Component {
+  state = {isSubscribed: false}
+
+  switchToFalse = () => {
+    this.setState(() => ({isSubscribed: false}))
+  }
+
+  switchToTrue = () => {
+    this.setState(() => ({isSubscribed: true}))
+  }
+
+  render() {
+    const {isSubscribed} = this.state
+    return (
+      <div className="container">
+        <h1 className="heading"> Welcome </h1>
+        <p className="description"> Thank you! Happy Learning</p>
+        {isSubscribed ? (
+          <button className="subButton" onClick={this.switchToFalse}>
+            Subscribed
+          </button>
+        ) : (
+          <button className="subButton" onClick={this.switchToTrue}>
+            Subscribe
+          </button>
+        )}
+      </div>
+    )
+  }
+}
+export default Welcome
